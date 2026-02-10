98737fbbf8701b3d7e5faca13a6f56078025c5ec
ry6969 <harrycanalita17@gmail.com>
2026-02-10 15:57:37 +0800

    Initial Commit

---
+++ PRACTICE ACTIVITY 1/CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter the amount in USD: ");
+double amountInUSD = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter exchange rate USD to EUR: ");
+double exchangeRate = Convert.ToDouble(Console.ReadLine());
+double amountInEUR = amountInUSD * exchangeRate;
+Console.WriteLine($"Amount in EUR: {amountInEUR:F2}");
+
---
+++ PRACTICE ACTIVITY 1/InterestCalculator/InterestCalculator.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter principal amount: ");
+float principalAmount = float.Parse(Console.ReadLine());
+Console.Write("Enter rate of interest: ");
+float rateOfInterest = float.Parse(Console.ReadLine());
+Console.Write("Enter time in years: ");
+float timeInYears = float.Parse(Console.ReadLine()); 
+
+float simpleInterest = (principalAmount * rateOfInterest * timeInYears) / 100;
+Console.WriteLine("Simple Interest: " + simpleInterest);
